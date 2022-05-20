74ced6e1a7f34127c7ea14b551db323b0f14db05
saanakkya1 <saanakkya123@gmail.com>
2022-05-20 14:52:08 +0530

    GOING TO IMPLEMENT A CLASS

--- DAY_1/009_printing_same_day_of_last_year.java
+++ DAY_1/009_printing_same_day_of_last_year.java
@@ -1,28 +1,38 @@
 import java.util.Calendar;
 public class printing_same_day_of_last_year {
     public static void main(String[] args){
-        Calendar now = Calendar.getInstance();
-        int DAY = now.get(now.DAY_OF_MONTH);
-        int MONTH = now.get(now.MONTH);
-        int YEAR = now.get(now.YEAR);
-        // now.get(now.DAY_OF_YEAR);
-        int H = now.get(now.HOUR);
-        int M = now.get(now.MINUTE);
-        int S = now.get(now.SECOND);
-        System.out.printf("LAST YEAR  SAME DAY : %02d %02d %02d %02d:%02d:%02d\n",DAY,MONTH+1,YEAR-1,H,M,S);
-        System.out.printf("LAST MONTH SAME DAY : %02d %02d %02d %02d:%02d:%02d\n",DAY,MONTH,YEAR,H,M,S);
-        System.out.printf("LAST Week  SAME DAY : %02d %02d %02d %02d:%02d:%02d\n",DAY,MONTH+1,YEAR,H,M,S);
-        
-
-        // now.add(now.YEAR, -1);
-        // // now.DAY_OF_WEEK;
-        // System.out.println(now.getTime());   
-        // now.add(now.YEAR, 1);
-        // now.add(now.MONTH,-1);
-        // System.out.println(now.getTime());
-        // now.add(now.MONTH,1);
-        // now.add(now.DAY_OF_YEAR,-7);
-        // System.out.println(now.getTime());
+        Calendar cal = Calendar.getInstance();
+        int year = Calendar.YEAR;
+        int month = Calendar.MONTH;
+        int day = Calendar.DAY_OF_MONTH;
+        cal.set(year-2,month-1,day);
+        printing_same_day_of_last_year.PrintCAL(cal,"Same Date Last Year");    
+  
     }
+    public static void PrintCAL(Calendar c,String s){
+        
+        int DAY = c.get(Calendar.DAY_OF_MONTH);
+        int MONTH = c.get(Calendar.MONTH);
+        int YEAR = c.get(Calendar.YEAR);
+        int H = c.get(Calendar.HOUR_OF_DAY);
+        int M = c.get(Calendar.MINUTE);
+        int S = c.get(Calendar.SECOND);
+        System.out.printf(s+" : %02d %02d %04d %02d:%02d:%02d\n",DAY,MONTH+1,YEAR,H,M,S);
+    }
+
     
 }
+/*public class print_cal{
+    public static void PrintCAL(Calendar c,String s){
+        
+        int DAY = c.get(Calendar.DAY_OF_MONTH);
+        int MONTH = c.get(Calendar.MONTH);
+        int YEAR = c.get(Calendar.YEAR);
+        int H = c.get(Calendar.HOUR_OF_DAY);
+        int M = c.get(Calendar.MINUTE);
+        int S = c.get(Calendar.SECOND);
+        System.out.printf(s+" : %02d %02d %04d %02d:%02d:%02d\n",DAY,MONTH+1,YEAR,H,M,S);
+   //     System.out.printf("LAST MONTH SAME DAY : %02d %02d %02d %02d:%02d:%02d\n",DAY,MONTH,YEAR,H,M,S);
+   //     System.out.printf("LAST Week  SAME DAY : %02d %02d %02d %02d:%02d:%02d\n",cal.DAY_OF_MONTH,cal.MONTH+1,YEAR,cal.HOUR,cal.MONTH,cal.SECOND);
+    }
+}*/
